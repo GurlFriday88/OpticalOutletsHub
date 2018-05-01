4aee328cfde27f49d9e9939c542544565ae9f34d
GurlFriday88 <entrotlek@gmail.com>
2018-04-30 23:51:48 -0400

    started new project, added AdminLTE template to project

--- OpticalOutletsHub/Controllers/HomeController.cs
+++ OpticalOutletsHub/Controllers/HomeController.cs
@@ -15,23 +15,23 @@ namespace OpticalOutletsHub.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult BCBS()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
 
+        public IActionResult ProviderNotes()
+        {
             return View();
         }
 
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
+
+
+
+
+
+
     }
 }
