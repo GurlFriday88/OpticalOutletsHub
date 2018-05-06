c6fcda2408a03566748702b08fc89378cc3974e5
GurlFriday88 <entrotlek@gmail.com>
2018-05-06 00:48:08 -0400

    reverted back to state where model classes were set up correctly. going to retry setting up db context

--- OpticalOutletsHub/Program.cs
+++ OpticalOutletsHub/Program.cs
@@ -20,6 +20,6 @@ namespace OpticalOutletsHub
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .Build();
+                .Build();   
     }
 }
